10b0509b412b1bb2a2a6f9cbd68ee6218ed29f02
danielev86 <danieleverr@gmail.com>
2019-09-01 20:19:35 +0200

    Refactoring

--- src/main/java/com/danielev86/aopdemoboot/service/impl/ProductServiceImpl.java
+++ src/main/java/com/danielev86/aopdemoboot/service/impl/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.danielev86.aopdemoboot.annotation.LogServiceAnnotation;
 import com.danielev86.aopdemoboot.provider.bean.ProductDTO;
 import com.danielev86.aopdemoboot.service.IProductService;
 import com.danielev86.aopdemoboot.service.mock.ProductSerciveMock;
