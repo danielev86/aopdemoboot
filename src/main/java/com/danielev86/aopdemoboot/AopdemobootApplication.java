062e88941dddcf55832445201f4ce45664da9768
danielev86 <danieleverr@gmail.com>
2019-09-01 09:16:37 +0200

    First commit

---
+++ src/main/java/com/danielev86/aopdemoboot/AopdemobootApplication.java
@@ -0,0 +1,13 @@
+package com.danielev86.aopdemoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopdemobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemobootApplication.class, args);
+	}
+
+}
